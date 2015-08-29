22ff1cf06bf13e08e80d636d7d1186db8895f082
amerriman <amm434@gmail.com>
2015-08-29 08:34:27 -0600

    tables populating correctly

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var submits = require('../models/items');
+var logic = require('../utilities/logic');
 
 var numUsers = 0;
 
@@ -22,7 +23,7 @@ router.post('/index', function(req, res){
     numUsers += 1;
    //!!!!!No slash on index!  It's knows its the html page!  AARARRRGGHHH!!!!
     res.render('index', {
-      numberOfUsers: "test",
+      //numberOfUsers: "test",
       numUsers: numUsers
     });
   }
@@ -30,11 +31,22 @@ router.post('/index', function(req, res){
 
 //go the the votes page to see the submissions and vote on them
 router.post('/vote', function(req, res){
-  //if(submits.submitArray[i] % 2)
+  var contestantsA = [];
+  var contestantsB = [];
+  for (var i = 0; i < submitsArray.length; i++) {
+    if (i % 2 === 0){
+      contestantsA.push(submitsArray[i]);
+    }
+    else {
+      contestantsB.push(submitsArray[i]);
+    }
+  }
   res.render('vote',
-    { submitsArray: submitsArray
+    { contestantsA: contestantsA,
+      contestantsB: contestantsB
     });
 });
+
 
 
 module.exports = router;
